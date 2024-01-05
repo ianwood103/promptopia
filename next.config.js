c0d59908a700319064e2d08202774dd5994f1ff7
Ian Wood <ianwood103@gmail.com>
2024-01-05 17:10:08 -0500

    update next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
-  image: {
-    domains: ["lh3.googleusercontent.com"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
   webpack(config) {
     config.experiments = {
