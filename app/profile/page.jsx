df170cb1331586eac2808f15dc59b5a0da039b97
Ian Wood <ianwood103@gmail.com>
2024-01-10 00:45:51 -0500

    finished edit and delete

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -10,9 +10,31 @@ const MyProfile = () => {
   const [posts, setPosts] = useState([]);
   const { data: session } = useSession();
 
-  const handleEdit = () => {};
+  const router = useRouter();
 
-  const handleDelete = async () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -22,9 +44,8 @@ const MyProfile = () => {
       setPosts(data);
     };
 
-    console.log(session);
     if (session?.user.id) fetchPosts();
-  }, []);
+  }, [session]);
 
   return (
     <Profile
